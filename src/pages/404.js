10d89aa156b1fbeb7181cd9e28307df4c8513a81
ZeroLifeCat <mandylogan8x@gmail.com>
2021-05-07 23:19:03 -0400

    aosdfjfd

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <div>
-     <SEO title="404: Not found" />
-     <h1>NOT FOUND</h1>
-     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-  
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
 )
 
 export default NotFoundPage
